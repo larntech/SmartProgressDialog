bf7d081c5c01b53458501aeccf7f2fc871d69aad
larntech <larntech20@gmail.com>
2020-08-18 21:59:34 +0300

    version 0.0.3
    Minor Updates

--- app/src/main/java/net/larntech/smartprogressdialog/MainActivity.kt
+++ app/src/main/java/net/larntech/smartprogressdialog/MainActivity.kt
@@ -2,12 +2,36 @@ package net.larntech.smartprogressdialog
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.widget.Button
+import net.larntech.showhideprogressdialog.ShowHideProgressDialog
 
 class MainActivity : AppCompatActivity() {
+    var  showProgressDialog: Button? = null;
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        showProgressDialog = findViewById(R.id.btnShowProgressDialog);
 
+        showProgressDialog!!.setOnClickListener {
+            showHideDialog();
+        }
 
     }
+
+
+
+
+    fun showHideDialog(){
+
+        ShowHideProgressDialog.show(this,"Processing request ..."); //show dialog
+
+        Handler().postDelayed(Runnable {
+            ShowHideProgressDialog.hide() //hide progress after 5 seconds
+        },5000);
+    }
+
 }
+
+

0f64fae217185446def8dec3ab4b0645b9d749f3
Richard kamere <richardkamere@Richards-MacBook-Pro.local>
2020-08-18 20:07:27 +0300

    first commit

---
+++ ShowHideProgressDialog/src/main/java/net/larntech/showhideprogressdialog/showHideProgressDialog.kt
@@ -0,0 +1,34 @@
+package net.larntech.showhideprogressdialog
+
+import android.app.ProgressDialog
+import android.content.Context
+
+object showHideProgressDialog {
+
+    var progressBar: ProgressDialog? = null;
+
+    fun show(show: Boolean,context: Context, message: String){
+
+        if(!progressBar!!.isShowing) {
+            progressBar = ProgressDialog(context)
+            progressBar!!.setCancelable(true) //you can cancel it by pressing back button
+
+            progressBar!!.setMessage(message)
+            progressBar!!.setProgressStyle(ProgressDialog.STYLE_SPINNER)
+            progressBar!!.progress = 0 //initially progress is 0
+
+            progressBar!!.max = 100 //sets the maximum value 100
+
+            progressBar!!.show() //displays the progress bar
+        }
+
+    }
+
+    fun hide(){
+        if(progressBar!!.isShowing){
+            progressBar!!.dismiss()
+        }
+    }
+
+
+}
